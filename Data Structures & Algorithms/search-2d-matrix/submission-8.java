aa37f0c028654f9c17ea0fd522e04829efce0ef5
Brilian Firdaus <brilianfird@gmail.com>
2026-04-02 23:04:58 +0700

    Add: search-2d-matrix - submission-8

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-8.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 1) return search2D(matrix[0], target);
+        int left = 0;
+        int right = matrix.length - 1;
+        int mid = 0;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (matrix[mid][0] < target) {
+                if (mid != matrix.length-1 && target > matrix[mid][0] && target < matrix[mid+1][0]) {
+                    return search2D(matrix[mid], target);
+                }
+                left++;
+            } else if (matrix[mid][0] > target) {
+                right--;
+            } else {
+                return true;
+            }
+        }
+        
+        return search2D(matrix[mid], target);
+    }
+
+    public boolean search2D(int[] matrix, int target) {
+        int left = 0;
+        int right = matrix.length-1;
+        while (left <= right) {
+            int mid = (left+right) / 2;
+            if (matrix[mid] < target) {
+                left++;
+            } else if (matrix[mid] > target) {
+                right--;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
