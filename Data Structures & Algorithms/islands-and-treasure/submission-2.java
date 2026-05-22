2ccf46f3b229a14a49ee340954ecf4362f425b93
Brilian Firdaus <brilianfird@gmail.com>
2026-05-22 10:11:45 +0700

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void islandsAndTreasure(int[][] grid) {
+        int[][] dirs = {
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+        };
+        Deque<int[]> queue = new ArrayDeque<>();
+        for (int i=0;i<grid.length;i++) {
+            for (int j=0;j<grid[i].length;j++) {
+                if (grid[i][j] == 0) {
+                    queue.offer(new int[]{i,j,0});
+                }
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int[] node = queue.poll();
+            int i = node[0];
+            int j = node[1];
+            int dist = node[2];
+            
+
+            for (int[] dir: dirs) {
+                int nextI = i+dir[0];
+                int nextJ = j+dir[1];
+
+                if (nextI < 0 || nextJ < 0 || nextI >= grid.length 
+                || nextJ >= grid[i].length) {
+                    continue;
+                }
+
+                if (grid[nextI][nextJ] != Integer.MAX_VALUE) {
+                    continue;
+                }
+                grid[nextI][nextJ] = dist+1;
+                queue.offer(new int[]{nextI, nextJ, dist+1});
+            }
+        }
+    }
+}
