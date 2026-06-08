536cc6ef50b9c0e7889953217d7c1154ea459597
Brilian Firdaus <brilianfird@gmail.com>
2026-06-08 08:49:15 +0700

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int jump(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return getMinJump(nums, 0, dp);
+    }
+
+    public int getMinJump(int[] nums, int currIdx, int[] dp) {
+        System.out.println(currIdx);
+        if (currIdx >= nums.length - 1) return 0;
+        else if (dp[currIdx] != -1) return dp[currIdx];
+        
+        dp[currIdx] = 1000;
+        for (int i=currIdx+1;i<=currIdx+nums[currIdx];i++) {
+            dp[currIdx] = Math.min(dp[currIdx], getMinJump(nums, i, dp) + 1);
+        }
+
+        return dp[currIdx];
+    }
+}
