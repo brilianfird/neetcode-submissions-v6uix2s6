c6e7b95d680aee1899e97f1802d3552f7231fa66
Brilian Firdaus <brilianfird@gmail.com>
2026-06-21 10:04:54 +0700

    Add: interleaving-string - submission-2

---
+++ Data Structures & Algorithms/interleaving-string/submission-2.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+
+        Boolean[][] dp = new Boolean[s1.length() + 1][s2.length() + 1];
+        return canInterleave(s1, s2, s3, "", 0, 0, dp);
+    }
+
+    public boolean canInterleave(String s1, String s2, String s3, String currString, int idx1, int idx2, Boolean[][] dp) {
+        if (currString.equals(s3)) {
+            return true;
+        } else if (idx1 == s1.length() && idx2 == s2.length()) {
+            return false;
+        } else if (dp[idx1][idx2] != null) {
+            return dp[idx1][idx2];
+        }
+
+        dp[idx1][idx2] = false;
+        if (idx1 < s1.length() && s1.charAt(idx1) == s3.charAt(idx1+idx2)) {
+            dp[idx1][idx2] = dp[idx1][idx2] || canInterleave(s1, s2, s3, currString + s1.charAt(idx1), idx1+1, idx2, dp);
+        }
+
+        if (idx2 < s2.length() && s2.charAt(idx2) == s3.charAt(idx1+idx2)) {
+            dp[idx1][idx2] = dp[idx1][idx2] || canInterleave(s1, s2, s3, currString + s2.charAt(idx2), idx1, idx2+1, dp);
+        }
+
+        return dp[idx1][idx2];
+
+    }
+}
